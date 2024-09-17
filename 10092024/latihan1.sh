a375458dbec042ef0b0413868921b16f12c6d28c
Xmeet789 <joshuamatthewhendra@gmail.com>
2024-09-17 09:23:12 +0700

    ini commit pertama

---
+++ 10092024/latihan1.sh
@@ -0,0 +1,68 @@
+#echo "Hitung Keliling Persegi Panjang"
+
+#echo "Masukkan Panjang Persegi Panjang:"
+#read panjang
+
+#echo "Masukkan lebar Persegi Panjang:"
+#read lebar
+
+ #echo "Keliling persegi panjang adalah $((2*( panjang + lebar)))"
+
+
+#echo "Hitung Volume Kerucut"
+
+#echo "Masukkan jari-jari alas kerucut:"
+#read r
+
+#echo "Masukkan tinggi kerucut:"
+#read t
+
+#volume=$( 0,3333333 * 3,14 * r * r * t )
+#echo "Volume kerucut adalah $volume"
+
+#untuk membuat function cukup ketikkan nama fungsi setelah fungsi, tanpa parameter
+#function nm_fungsi
+
+#untuk memanggil function cukup panggil nama function nya saja
+#nm_fungsi
+
+
+#function gaya {
+    #echo "Script untuk menghitung gaya"
+
+    #echo "Masukkan massa benda (dalam satuan kg)"
+    #read m
+
+    #echo "Masukkan percepatan benda (dalam satuan m/s)"
+    #read a
+
+    #gaya=$( m * a )
+    #echo "Jadi gayanya adalah $gaya"
+#}
+
+
+#gaya
+
+#Studi kasus 1 - Menghitung budget pengembangan aplikasi
+echo "Masukkan jumblah pengembang aplikasi"
+read developers
+
+echo "Masukkan gaji developer per bulan, per 1 orang"
+read monthly_salary
+
+echo "Masukkan lama proyek (dalam bulan)"
+read project_length
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 10092024/scriptPertama.sh
@@ -0,0 +1,9 @@
+#echo "Nama saya adalah Martin";
+
+#command ini untuk berinteraksi dengan terminal
+#Setelah read merupakan nama variabel untuk menampung jawaban atau interaksi antara user dan terminal 
+
+#read name;
+#Untuk memanggil variabel yang kita gunakan untuk menampung gunakan "$"
+#echo "Nama saya adalah $name ";
+
